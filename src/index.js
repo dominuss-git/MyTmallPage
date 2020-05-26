15aac975b1221def0c5745fbd8243da5b0f6c97b
Dominuss-afk <kiril6701@gmail.com>
2020-05-26 11:04:34 +0300

    grid/line swapper

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 import $ from 'jquery'
 import './css/style.css'
+
+$('.throw-line__lines').click(function(e) {
+    $('.devices').css('flex-direction', 'column');
+    $('.img-box').css('width', '200px');
+    $('.devices__item').css('flex-direction', 'row');
+    $('.title__t').css('width', '400px');
+    $('.title__t').css('padding', '15px 0 0 0');
+    $('.cost').css('padding', '35px 0 0 0');
+    $('.devices__text1').text("Доставка из: Россия");
+    $('.devices__text2').text("Приблизительно: дн.");
+})
+
+$( window ).resize(function() {
+    if( $( window ).width() <= 1150) {
+        $('.throw-line__grid').click();
+    }
+});
+
+$('.throw-line__grid').click(function(e) {
+    $('.devices').css('flex-direction', 'row');
+    $('.img-box').css('width', '200px');
+    $('.devices__item').css('flex-direction', 'column');
+    $('.title__t').css('width', '200px');
+    $('.title__t').css('padding', '0');
+    $('.cost').css('padding', '0');
+    $('.devices__text1').text("");
+    $('.devices__text2').text("");
+})
